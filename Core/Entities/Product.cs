4da0ff886e641da32c095b3aac31dc937cb9762e
bryanbocio <alexanderbocio@gmail.com>
2022-11-05 19:49:22 -0400

    modificando la entidad principal Product agregandoles nuevos feactures

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -8,6 +8,16 @@ namespace Core.Entities
     public class Product : BaseEntity
     {
         public string name { get; set; }
-    
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public string pictureUrl { get; set; }
+        public ProductType productType { get; set; }
+        public int productTypeId {get; set;}
+
+        public ProductBrand productBrand { get; set; }
+
+        public int productBrandId { get; set; }
+
+
     }
 }
