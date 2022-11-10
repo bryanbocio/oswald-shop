3edb6446cb403632af1d2e81be057359949a086c
bryanbocio <alexanderbocio@gmail.com>
2022-11-09 22:10:14 -0400

    Implementando clase generica de repositories en un repository genérico, (Uso de Generics)

---
+++ Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _storeContext;
+
+        public GenericRepository(StoreContext storeContext)
+        {
+            this._storeContext = storeContext;
+        }
+
+        public async Task<T> getByIdAsync(int id)
+        {
+            var entity=await _storeContext.Set<T>().FindAsync(id);
+
+            return entity;
+        }
+
+        public async Task<IReadOnlyList<T>> listAllAsync()
+        {
+            return await _storeContext.Set<T>().ToListAsync();
+        }
+    }
+}
