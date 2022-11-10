ee6d8cb18340f68ee6a88fa1eb4b966de0b64d16
bryanbocio <alexanderbocio@gmail.com>
2022-11-09 22:09:46 -0400

    Agregando interfaz genérica para los repositories

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> getByIdAsync(int id);
+        Task<IReadOnlyList<T>> listAllAsync();
+    }
+}
