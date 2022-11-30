392aa85eb9ef4b5736c975dc5ab4ec193e3bafa3
bryanbocio <alexanderbocio@gmail.com>
2022-11-29 21:36:54 -0400

    Agregando Interfaz para implementacion de Specification Pattner

---
+++ Core/Specification/Specification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    internal interface Specification<T>
+    {
+
+        Expression<Func<T, bool>> criteria { get; }
+        List<Expression<Func<T, object>>> include { get; }
+    }
+}
