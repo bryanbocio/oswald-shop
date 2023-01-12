372cdfefc608bdc0cb618d29fa278a084d5c022f
bryanbocio <alexanderbocio@gmail.com>
2023-01-12 19:50:00 -0400

    adding default messages for the apiResponse class

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -6,10 +6,22 @@
 
         public string Message { get; set; }
 
-        public ApiResponse(int statusCode, string message)
+        public ApiResponse(int statusCode, string message=null)
         {
             StatusCode = statusCode;
-            Message = message;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorized, you are not",
+                404 => "Resource found, it was not",
+                500 => "Errors are the path to the dark side. Errors lead to anger. Anger leads to hate. Hate leads to career change",
+                _ => null
+            };
         }
     }
 }
