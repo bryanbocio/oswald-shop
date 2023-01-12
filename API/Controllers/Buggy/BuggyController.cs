e8531ee1b93250a90d83d23211651a22d263aa8a
bryanbocio <alexanderbocio@gmail.com>
2023-01-12 19:42:29 -0400

    adding method for testing errors api response

--- API/Controllers/Buggy/BuggyController.cs
+++ API/Controllers/Buggy/BuggyController.cs
@@ -1,7 +1,51 @@
-﻿namespace API.Controllers.Buggy
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Buggy
 {
     public class BuggyController:BaseApiController
     {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var thing = _context.Products.Find(42);
+
+            if(thing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+
+            var thing = _context.Products.FindAsync(42);
+
+            var thingTOreturn= thing.ToString();
+
+            return Ok();
+        }
+
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+           return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
     }
 }
