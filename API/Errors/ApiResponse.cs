d2e5308b0e7e8cc46cd443003473f625f7e3b7ab
bryanbocio <alexanderbocio@gmail.com>
2023-01-12 19:45:20 -0400

    creating apiResponse class

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public ApiResponse(int statusCode, string message)
+        {
+            StatusCode = statusCode;
+            Message = message;
+        }
+    }
+}
