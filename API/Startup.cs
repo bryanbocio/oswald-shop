7c62fcf6844c6f5e834c6f15f98a039cfa04ad35
bryanbocio <alexanderbocio@gmail.com>
2023-01-12 20:22:10 -0400

    adding Error services to the startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,6 +56,8 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
+            app.UseStatusCodePagesWithReExecute("/erros/{0}");
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
