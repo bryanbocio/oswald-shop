31b0eb8b1af5ede1f7dd2c346a67aa65f4b55a1a
bryanbocio <alexanderbocio@gmail.com>
2023-01-12 20:18:51 -0400

    creating a ErrorController

---
+++ API/Controllers/Error/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using API.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Error
+{
+
+    [Route("erros/{code}")]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int statusCode)
+        {
+            return new ObjectResult(new ApiResponse(statusCode));
+        }
+    }
+}
