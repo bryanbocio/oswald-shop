78f80b57a07e0353130b809d8019690f207ef1b2
bryanbocio <alexanderbocio@gmail.com>
2023-01-15 16:57:28 -0400

    implementing Orderby inside ProductSpecification class

--- Core/Specification/Products/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/Products/ProductsWithTypesAndBrandsSpecification.cs
@@ -10,10 +10,27 @@ namespace Core.Specification.Products
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(string sort)
         {
             AddInclude(product => product.productType);
             AddInclude(product => product.productBrand);
+            AddOrderBy(product => product.name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(product => product.price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(product => product.price);
+                        break;
+                    default:
+                        AddOrderBy(product=>product.name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id):base(product=>product.id==id)
