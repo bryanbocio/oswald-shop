c1ee19acbb12c238ab0f7dd86b5d8da6e820d95a
bryanbocio <alexanderbocio@gmail.com>
2023-01-15 17:57:15 -0400

    Adding class for storing all parameters

---
+++ Core/Specification/Products/ProductSpecificationParameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification.Products
+{
+    public class ProductSpecificationParameters
+    {
+        private const int MaxPageSize = 50;
+        public int PageIndex { get; set; } = 1;
+
+        private int _pagSize = 6;
+
+        public int PageSize 
+        { 
+            get => _pagSize; 
+            set => _pagSize = (value>MaxPageSize)?MaxPageSize: value; 
+        }
+
+        public int? BrandId { get; set; }
+        public int? TypeId { get; set; }
+        public string? Sort { get; set; }
+
+
+
+    }
+}
