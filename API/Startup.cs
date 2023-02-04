088b58aa795bfced97a2d5d0c68f7a35d41de2a5
bryanbocio <alexanderbocio@gmail.com>
2023-02-04 14:09:29 -0400

    editing origins from Cors Policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,7 +29,7 @@ namespace API
             {
                 options.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 });
             });
 
