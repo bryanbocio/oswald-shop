a03af34965494d8bc03fbf696bf66ac785dfc3fd
bryanbocio <alexanderbocio@gmail.com>
2023-03-05 17:09:27 -0400

    adding register dto

---
+++ API/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.Account
+{
+    public class RegisterDto
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
