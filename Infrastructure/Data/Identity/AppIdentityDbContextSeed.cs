16735f367cf9eeda7158cbd028658e70b8e41f2a
bryanbocio <alexanderbocio@gmail.com>
2023-03-05 14:05:32 -0400

    adding seed data class for identity

---
+++ Infrastructure/Data/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Bryan",
+                    Email = "bbocio@test.com",
+                    UserName = "bbocio@test.com",
+                    Address = new Address
+                    {
+                        FirstName ="Bryan",
+                        LastName = "Bocio",
+                        Street = "street 4th, #289 Los mameyes, SDE",
+                        City = "Santo Domingo Este",
+                        State = "Santo Domingo Este",
+                        ZipCode ="10023"
+                    }
+                };
+                await userManager.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
