d9c7edbe180b22c08399b6ebcfbe953e8f7fb906
bryanbocio <alexanderbocio@gmail.com>
2023-03-05 13:45:26 -0400

    creating identity service extensions class for adding a db context as a services and using new string connection for identity

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection service,IConfiguration config)
+        {
+            service.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+
+            return service;
+        }
+    }
+}
