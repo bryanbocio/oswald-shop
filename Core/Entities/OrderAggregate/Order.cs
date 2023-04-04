c8a62e8ede5cda23261e16f446a2b4b199438318
bryanbocio <alexanderbocio@gmail.com>
2023-04-04 19:57:05 -0400

    adding Order Entity

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+
+        public Order(string buyerEmail,
+                     Address shipToAddress, 
+                     DeliveryMethod deliveryMethod, 
+                     IReadOnlyList<OrderItem> orderItems,
+                     decimal subTotal)
+        {
+
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            SubTotal = subTotal;
+        }
+
+        public string BuyerEmail { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+
+        public Address ShipToAddress { get; set; }
+
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems{ get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        public string PaymentItentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return SubTotal + DeliveryMethod.Price;
+        }
+    }
+}
