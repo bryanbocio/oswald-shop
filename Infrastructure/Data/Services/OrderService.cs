5122cc5e8b57689d744c99c24f8d4455ecf80647
bryanbocio <alexanderbocio@gmail.com>
2023-04-04 21:38:15 -0400

    implementing Orderservice interface in OrderService class

---
+++ Infrastructure/Data/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+
+namespace Infrastructure.Data.Services
+{
+    public class OrderService : IOrderService
+    {
+
+        public OrderService()
+        {
+
+        }
+
+        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+        {
+
+        }
+
+        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
