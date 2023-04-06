7ac723e53489174078b612d3169cb57ab18f79c5
bryanbocio <alexanderbocio@gmail.com>
2023-04-06 19:43:29 -0400

    Adding specifications for Orders

---
+++ Core/Specification/Orders/OrdersWithItemsAndOrderingSpecification.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification.Orders
+{
+    public class OrdersWithItemsAndOrderingSpecification :BaseSpecification<Order>
+    {
+
+        public OrdersWithItemsAndOrderingSpecification(string email):base(order=> order.BuyerEmail==email)
+        {
+            AddInclude(order => order.OrderItems);
+            AddInclude(order => order.DeliveryMethod);
+            AddOrderByDescending(order => order.OrderDate);
+        }
+
+        public OrdersWithItemsAndOrderingSpecification(int id, string buyerEmail) : base(order=> order.id==id && order.BuyerEmail==buyerEmail)
+        {
+            AddInclude(order => order.OrderItems);
+            AddInclude(order => order.DeliveryMethod);
+        }
+
+    }
+}
