985b6aaca036b315d1551e216b205eadf0068ad6
bryanbocio <alexanderbocio@gmail.com>
2023-04-06 14:59:16 -0400

    adding orderDto

---
+++ API/DTOs/Order/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using API.DTOs.Account;
+
+namespace API.DTOs.Order
+{
+    public class OrderDto
+    {
+        public string BasketId { get; set; }
+
+        public int DeliveryMethodId { get; set; }
+        public AddressDto ShipToAddress{ get; set; }
+    }
+}
