dc08cd0447ac72c5cf95228d089db1732cda1983
bryanbocio <alexanderbocio@gmail.com>
2023-04-06 19:40:30 -0400

    adding a new migration, modifying a field from Order entity

---
+++ Infrastructure/Data/Migrations/20230406232214_OrderEntityModifyied.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class OrderEntityModifyied : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentItentId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntentId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentItentId",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
