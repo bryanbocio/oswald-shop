09593b818b457ff9586076f72564bd64315709ab
bryanbocio <alexanderbocio@gmail.com>
2023-04-08 12:22:00 -0400

    adding more mappings profiles

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,9 +1,11 @@
 ﻿using API.DTOs;
 using API.DTOs.Account;
 using API.DTOs.Customer;
+using API.DTOs.Order;
 using AutoMapper;
 using Core.Entities;
 using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -20,13 +22,23 @@ namespace API.Helpers
 
                      .ForMember(productDto => productDto.pictureUrl, options => options.MapFrom<ProductUrlResolver>());
 
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
 
             CreateMap<CustomerBasketDto, CustomerBasket>();
             
             CreateMap<BasketItemsDto, BasketItems>();
 
             CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+
+            CreateMap<Order, OrderToReturnDto>()
+                     .ForMember(order => order.DeliveryMethod, options => options.MapFrom(source => source.DeliveryMethod.ShortName))
+                     .ForMember(order => order.ShippingPrice,  options => options.MapFrom(source => source.DeliveryMethod.Price));
+
+            CreateMap<OrderItem, OrderItemDto>()
+                     .ForMember(order => order.ProductId,   options => options.MapFrom(source => source.ItemOrdered.ProductItemId))
+                     .ForMember(order => order.ProductName, options => options.MapFrom(source => source.ItemOrdered.ProductName))
+                     .ForMember(order => order.PictureUrl,  options => options.MapFrom(source => source.ItemOrdered.PictureUrl))
+                     .ForMember(order => order.PictureUrl,  options => options.MapFrom<OrderItemUrlResolver>());
 
 
         }
