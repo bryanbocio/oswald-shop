f4878f86106ce79d82bbc56a4238a8dce86d9df2
bryanbocio <alexanderbocio@gmail.com>
2023-04-08 12:22:30 -0400

    adding a new UrlResolver for OrderDto's pictureUrl

---
+++ API/Helpers/OrderItemUrlResolver.cs
@@ -0,0 +1,26 @@
+﻿using API.DTOs.Order;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+
+namespace API.Helpers
+{
+    public class OrderItemUrlResolver : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        private readonly IConfiguration _configuration;
+
+        public OrderItemUrlResolver(IConfiguration config)
+        {
+            _configuration=config;
+        }
+
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.ItemOrdered.PictureUrl))
+            {
+                return _configuration["ApiUrl"] + source.ItemOrdered.PictureUrl;
+            }
+
+            return String.Empty;
+        }
+    }
+}
