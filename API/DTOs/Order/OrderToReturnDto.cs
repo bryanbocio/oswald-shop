211a33cfe0204123d185b2527ebe5f916ad5b5ea
bryanbocio <alexanderbocio@gmail.com>
2023-04-08 12:21:39 -0400

    Adding orderToReturnDTO

---
+++ API/DTOs/Order/OrderToReturnDto.cs
@@ -0,0 +1,28 @@
+﻿
+using Core.Entities.OrderAggregate;
+
+namespace API.DTOs.Order
+{
+    public class OrderToReturnDto
+    {
+        public int id { get; set; }
+        public string BuyerEmail { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public Address ShipToAddress { get; set; }
+
+        public string DeliveryMethod { get; set; }
+
+        public decimal ShippingPrice { get; set; }
+
+        public IReadOnlyList<OrderItemDto> OrderItems { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+        public decimal Total { get; set; }
+
+        public string Status { get; set; } 
+
+    }
+}
